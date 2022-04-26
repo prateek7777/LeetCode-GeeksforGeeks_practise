62f387ae51507bdddff2e1382fed9bc2f4df9379
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-04-26 16:15:58 +0530

    Time: 2 ms (81.70%), Space: 56.3 MB (40.09%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -4,18 +4,20 @@ class Solution {
         int consider_max = 0;
         
         //Moore's Voting Algorithm
-        
+//consider nums = [2,2,2,3,4]. consider_max won't change, because count will never reach zero. it will be 0,1,2,3,2,1.
+//Thus, Moore's Voting Algorithm works.        
         for(int num : nums){
             
             if(count==0)
                 consider_max = num;
             
             if(consider_max==num)
-                count--;
-            else
                 count++;
+            else
+                count--;
         
         }
+        
         return consider_max;
                     
         }
