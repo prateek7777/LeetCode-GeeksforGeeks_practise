35d67fd360f343d175101526b28d606f522fd40a
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-04-28 23:33:14 +0530

    Time: 112 ms (34.29%), Space: 51.5 MB (71.23%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.java
+++ 493-reverse-pairs/493-reverse-pairs.java
@@ -4,33 +4,33 @@ class Solution {
     }
     
     public int mergeSort(int[] nums, int low, int high){
-        if(low>=high)
+        if(low==high)                                              //base case
         return 0;
-        int mid = (low+high)/2;
-        int f_count = mergeSort(nums, low, mid);
-        f_count = f_count + mergeSort(nums, mid+1, high);
-        f_count = f_count + merge(nums, low, mid, high);
+        int mid = (low+high)/2;                                    //not (nums.length)/2
+        int f_count = mergeSort(nums, low, mid);                   //count from the left half.
+        f_count = f_count + mergeSort(nums, mid+1, high);          //count from the right half.
+        f_count = f_count + merge(nums, low, mid, high);           //overall count from both left and right halves.
         
         return f_count;
     }
     
     public int merge(int[] nums, int low, int mid, int high){
         int count = 0;
-        int j = mid+1;
-        for(int i = low; i<=mid; i++)
+        int j = mid+1;                                             //j is for the right half. It ranges from (mid+1) to high.
+        for(int i = low; i<=mid; i++)                              //use a for loop. Not a while loop. i is for the first half. It ranges from low to mid.
         {
-            while(j<=high && nums[i]>(2 * (long) nums[j])){
+            while(j<=high && nums[i]>(2 * (long) nums[j])){        //The condition that has to be satisfied.
                 j++;
             }
-            count = count + (j-(mid+1));
+            count = count + (j-(mid+1));                           //Since j is getting incremented on the basis of the given condition, j-(mid+1) will be having the required count.
         }
         
+        //Merge Sort.
         ArrayList<Integer> sorted = new ArrayList<Integer>();
-        
         int left = low;
         int right = mid+1;
         
-        while(left<=mid && right<=high){
+        while(left<=mid && right<=high){                  //main sorting.
             if(nums[left]<=nums[right]){
             sorted.add(nums[left++]);
             }
@@ -40,16 +40,16 @@ class Solution {
         }
         
         while(left<=mid)
-        sorted.add(nums[left++]);
+        sorted.add(nums[left++]);                         //leftover left elements.
         
         while(right<=high)
-        sorted.add(nums[right++]);
+        sorted.add(nums[right++]);                        //leftover right elements.
         
         for(int i=low; i<=high; i++){
-            nums[i] = sorted.get(i-low);
+            nums[i] = sorted.get(i-low);                 //return type is array. So, copy all the elements of the list to the array.
         }
         
-    return count;
+    return count;                                        //return the final count.
     }
     
     
