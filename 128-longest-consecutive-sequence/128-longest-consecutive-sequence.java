3c0cae1aba8078e5f687bc59d3ef43dbbee9da4b
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-01 01:02:15 +0530

    Time: 684 ms (6.92%), Space: 156.2 MB (6.37%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -2,26 +2,26 @@ class Solution {
     public int longestConsecutive(int[] nums) {
         Set<Integer> hashSet = new HashSet<Integer>();
         for(int num : nums)
-            hashSet.add(num);
+            hashSet.add(num);                           //populating the hashSet.
         
-        int longest_Streak = 0;
-        int curr_Streak = 1;
-        for(int num : nums){
+        int longest_Streak = 0;                         
+        int curr_Streak = 0;
+        for(int num : nums){                            //num = 2. nums = [2,3,4,5]
             
-            if(!hashSet.contains(num-1)){
-                int curr_Num = num;
-                curr_Streak = 1;
+            if(!hashSet.contains(num-1)){              //if hashSet doesn't contain 1. 
+                int curr_Num = num;                    //curr_Num = 2.
+                curr_Streak = 1;                       //curr_Streak count will be 1, because 2(curr_Num) is a part of the streak.
             
-            while(hashSet.contains(curr_Num+1)){
-                curr_Num = curr_Num+1;
-                curr_Streak = curr_Streak+1;
-            }
+            while(hashSet.contains(curr_Num+1)){       //while hashSet contains 3.4.5.            6.
+                curr_Num = curr_Num+1;                 //curr_Num = 3.4.5.
+                curr_Streak = curr_Streak+1;           //curr_Streak = 2.3.4.
+            } 
                 
             }
             
-            longest_Streak = Math.max(curr_Streak, longest_Streak);
+            longest_Streak = Math.max(curr_Streak, longest_Streak); //Math.max(0,4) = 4.
             
         }
-        return longest_Streak;
+        return longest_Streak;   //4.
     }
 }
