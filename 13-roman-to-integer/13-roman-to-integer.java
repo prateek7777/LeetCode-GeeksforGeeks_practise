ad4d93305f9cc7771eab902c0698ab46bc52a3f4
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-02 18:24:58 +0530

    Time: 6 ms (72.77%), Space: 42.9 MB (76.47%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,20 +1,20 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character, Integer> convert = new HashMap<Character, Integer>();
-        convert.put('I', 1);
-        convert.put('V', 5);
-        convert.put('X', 10);
-        convert.put('L', 50);
-        convert.put('C', 100);
-        convert.put('D', 500);
-        convert.put('M', 1000);
+        HashMap<Character, Integer> convertMap = new HashMap<Character, Integer>();
+        convertMap.put('I', 1);             //put the values as given in the question.
+        convertMap.put('V', 5);
+        convertMap.put('X', 10);
+        convertMap.put('L', 50);
+        convertMap.put('C', 100);
+        convertMap.put('D', 500);
+        convertMap.put('M', 1000);
         
-        int roman = convert.get(s.charAt(s.length()-1));
-        for(int i = s.length()-2; i>=0; i--){
-            if(convert.get(s.charAt(i)) < convert.get(s.charAt(i+1)))
-                roman = roman - convert.get(s.charAt(i));
-            else
-                roman = roman + convert.get(s.charAt(i));
+        int roman = convertMap.get(s.charAt(s.length()-1));              //the very last character in the input string.
+        for(int i = s.length()-2; i>=0; i--){               //we start iterating from the second last char in the input string.
+            if(convertMap.get(s.charAt(i)) < convertMap.get(s.charAt(i+1)))  //1 < 5 => IV.
+                roman = roman - convertMap.get(s.charAt(i));                 //5 - 1 = 4.
+            else                                                             //5 > 1 => VI.
+                roman = roman + convertMap.get(s.charAt(i));                 //5 + 1 = 6.
         }
         return roman;
     }
