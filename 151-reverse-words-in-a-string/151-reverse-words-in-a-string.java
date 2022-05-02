a1f620912a6ad10c16818793bbdde5727a176b6f
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-02 23:02:53 +0530

    Time: 15 ms (28.19%), Space: 54 MB (15.26%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,5 +1,5 @@
 class Solution {
-    public String reverseWords(String s) {
+    public String reverseWords(String s) {             //Hello World!
         int n = s.length();
         int i = 0;
         int j = 0;
@@ -7,29 +7,29 @@ class Solution {
         String result = "";
         while(i<n){
             
-            while(i<n && s.charAt(i) == ' ')
+            while(i<n && s.charAt(i) == ' ')           //iterate through white space between the words.
                 i++;
             
-            if(i==n)
+            if(i==n)                                   //if i == n, you cannot do j = i+1. It causes stack overflow error.
             break;
             
-            j = i+1;
+            j = i+1;                                   //i will stop at the very end of white space. immediately after i, j will iterate through each individual word.
             
-            while(j<n && s.charAt(j) != ' ')
+            while(j<n && s.charAt(j) != ' ')           //j will iterate through each word until it reaches a white space.
                 j++;
             
-            result = s.substring(i, j);
+            result = s.substring(i, j);                //result will store each word which will range from i to j. Hello; World!
             
-            i = j+1;
+            i = j+1;                                   //i again goes for the next white space and the loop continues till the end of the string.
             
-            if(f_result.length() == 0)
-                f_result = result;
+            if(f_result.length() == 0)                 //the very first word will be put in f_result.
+                f_result = result;                     //Hello
             else
-                f_result = result + " " + f_result;
-         
+                f_result = result + " " + f_result;    //we can clearly see the reversing of the words. f_result is being put after the result. //World! Hello
+                                     
         }
         
-        return f_result;
+        return f_result;                               
         
     }
 }
