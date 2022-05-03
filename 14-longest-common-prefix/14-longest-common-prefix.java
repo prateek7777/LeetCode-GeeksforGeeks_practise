44ca26aa36fe02c76feaa09cc6b8b8d4ee023064
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-03 18:31:10 +0530

    Time: 1 ms (81.20%), Space: 42.9 MB (13.52%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.java
+++ 14-longest-common-prefix/14-longest-common-prefix.java
@@ -3,17 +3,17 @@ class Solution {
         int n = strs.length;
         int i = 0;
         String prefix = "";
-        prefix = strs[0];
-        
+        prefix = strs[0];   //consider the entire first word of the string array as prefix.
+        // .indexOf() returns 0 for matching, returns -1 for not matching.
         for(i = 1; i<n; ){
-        
-            if(strs[i].indexOf(prefix) != 0)
-                prefix = prefix.substring(0, prefix.length()-1);
+        //indexOf() function compares string at strs[i] and prefix string.
+            if(strs[i].indexOf(prefix) != 0)                      //while(strs[i].indexOf(prefix) != 0) can also be used instead of if-else.
+                prefix = prefix.substring(0, prefix.length()-1);  //chop the prefix by 1 if the function returns -1.
             else
-                i++;
+                i++;   //if the function returns 0(prefix is matching), iterate ahead without chopping the string prefix.
             
         }
-        
+    
         return prefix;
         
     }
