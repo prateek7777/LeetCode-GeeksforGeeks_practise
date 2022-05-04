68366d4d90df40559d7e44286c8ad25f1fc8a9e9
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-04 16:49:07 +0530

    Time: 4 ms (76.88%), Space: 43.4 MB (65.64%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length() != t.length())
+        if(s.length() != t.length()) //obviously, if the count of chars is not same, it is impossible for two the strings to be anagrams.
             return false;
         
-         char[] char_counter = new char[26];
+         char[] char_counter = new char[26];              //the char[] array for 26 alphabets.
         
         for(int i = 0; i<s.length(); i++){
-            
-           char_counter[s.charAt(i) - 'a']++;
-           char_counter[t.charAt(i) - 'a']--;
-            
+        //consider s = "anagram" , t = "nagaram".
+           char_counter[s.charAt(i) - 'a']++;     //char['a'-'a'] = char[0]++ = 0+1 = 1. char_counter[0] = 1.
+           char_counter[t.charAt(i) - 'a']--;     //char['a'-'a'] = char[0]-- = 1-1 = 0. char_counter[0] = 0.
+            //char_counter[s.charAt(i) - 'a']. (- 'a') returns the index.
         }
         
-        int sum = 0;
-        for(int ch : char_counter)
-        {
-            sum = sum + ch;
-        }
-        
-        if(sum==0)
-            return true;
+        int sum = 0;                                   //Alternatively.
+        for(int ch : char_counter)                     //for(int ch : char_counter)
+        {                                               //{
+            sum = sum + ch;            //0+0 = 0.      //if(ch!=0)
+        }                                              //return false;
+                                                       //}
+        if(sum==0)                                     //return true;
+            return true;     
         else
-            return false;
+            return false; 
     }
 }
