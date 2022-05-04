eb4076bae350cbb3be7d4f48a1147c9aff48ae35
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-04 12:05:55 +0530

    Time: 0 ms (100.00%), Space: 40.5 MB (87.20%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -3,20 +3,19 @@ class Solution {
         int h = haystack.length();
         int n = needle.length();
         
-        if(n==0)
+        if(n==0)      //as per the question, if the needle is an empty string, we should return 0.
             return 0;
         
-        if(n>h)
+        if(n>h)           //a needle larger than the haystack itself cannot exist in the haystack.
             return -1;
-        
-        for(int i = 0; i<h-n+1; i++){
-            
-            if(haystack.substring(i, i+n).equals(needle))
+        //consider haystack = "Hello", needle = "ll".
+        for(int i = 0; i<h-n+1; i++){ //i<h => traverse through the entire haystack; i<h-m => traverse in haystack from H to l(index 2). But we need to go until l(index 3). ; i<h-n+1 => traverse from                                         //H to l(index 3) as per the requirement.
+            if(haystack.substring(i, i+n).equals(needle)) // (He).equals(ll); (el).equals(ll); (ll).equals(ll) return 2.
                 return i;
             
             }
         
-        return -1;
+        return -1;        //after traversing through the entire loop, if there is no needle found in the haystack, return -1.
         
     }
 }
