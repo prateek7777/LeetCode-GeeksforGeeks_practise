86987d00d5278ba38707e06779fddf8bb8e728b9
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-06 01:00:19 +0530

    Time: 1 ms (91.06%), Space: 41.2 MB (74.18%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -3,7 +3,7 @@ class Solution {
         
         int v1 = 0;
         int v2 = 0;
-        int l1 = version1.length();
+        int l1 = version1.length();         
         int l2 = version2.length();
         int i = 0;
         int j = 0;
@@ -13,17 +13,17 @@ class Solution {
             v1 = 0;
             v2 = 0;
             
-            while(i<l1 && version1.charAt(i) != '.'){
-            v1 = v1*10 + version1.charAt(i) - '0';  
-            i++;
+            while(i<l1 && version1.charAt(i) != '.'){            //iterate through the version1 string, while there is no '.' in the string.
+            v1 = v1*10 + version1.charAt(i) - '0'; //version1.charAt(i) - '0' will give the numerical value difference as version1 is a string containing chars and we are substracting 0 in its char                                                        //format('0'). the numerical value difference is the actual number in that place(char) of the string.
+            i++;      
             }
             
-            while(j<l2 && version2.charAt(j) != '.'){
+            while(j<l2 && version2.charAt(j) != '.'){            //same logic as version1 string. This is for version2.
             v2 = v2*10 + version2.charAt(j) - '0';    
             j++;
             }
         
-            if(v1>v2)
+            if(v1>v2)                                            //as per the problem statement.
                 return 1;
             else if(v1<v2)
                 return -1;
@@ -35,7 +35,7 @@ class Solution {
             
             }
         
-        return 0;
+        return 0;                                                //coming out of the while loop implies that v1=v2.
         
         }
     }
