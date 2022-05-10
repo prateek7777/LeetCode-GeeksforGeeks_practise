34a051c3b4891a2318e7ef1f495e2c31467f9e35
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-10 13:52:45 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (95.13%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -10,37 +10,39 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode tempNode = new ListNode(0);
-        ListNode currNode = tempNode;
+        ListNode tempNode = new ListNode(0);                                 //see last    
+        ListNode currNode = tempNode;                                        //see last
         
-        while(list1 != null && list2 != null){
+        while(list1 != null && list2 != null){                               //iterating through both list1 and list2.
             
             if(list1.val < list2.val){
-                currNode.next = list1;
-                list1 = list1.next;
-                 currNode = currNode.next;
+                currNode.next = list1;                                       //not currNode = list1. (this is a linked list, not an array). assigning.
+                list1 = list1.next;                                          //iterate as usual.
+                currNode = currNode.next;                                    //iterate as usual.
             }
             
             else{
-                currNode.next = list2;
-                list2 = list2.next;
-                currNode = currNode.next;
+                currNode.next = list2;                                       //not currNode = list2. (this is a linked list, not an array). assigning.
+                list2 = list2.next;                                          //iterating.
+                currNode = currNode.next;                                
             }   
             
         }
         
-        if(list1 != null){
-            currNode.next = list1;
-            list1 = list1.next;
+        if(list1 != null){                                                   //in case list2 became null before list1 and the while loop got broken.
+            currNode.next = list1;                                           //assigning.
+            list1 = list1.next;                                              //iterating.
             currNode = currNode.next;
         }
         
-        if(list2 != null){
-            currNode.next = list2;
-            list2 = list2.next;
+        if(list2 != null){                                                   //in case list1 became null before list2 and the while loop got broken.
+            currNode.next = list2;                                           //assigning.
+            list2 = list2.next;                                              //iterating.
              currNode = currNode.next; 
         }
         
-        return tempNode.next;
+        return tempNode.next;                                                //see last.
+        //see last.
+        //When we iterate through the while loop, we lose what last node is since we can only look at one node at a time. Think about it like a long line of cars, 1st car is yellow, 2nd car is blue, 3rd car is red. We assign the variable currNode to yellow, blue and red as we iterate. When we get to red, currNode is assigned to red, and we have no way of knowing where the yellow car is anymore. However, if we put a green car in front(green, yellow, blue, red) of the yellow car, we can find out where the yellow car is again, which is just greencar.next. The green car in this case is the tempNode. the yellow car is the actual head that we have to return after merging the two sorted linked lists.
     }
 }
