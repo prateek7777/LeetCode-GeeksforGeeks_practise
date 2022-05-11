084c7cf0625dee8db09d3b053dec8fcded729c5c
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-11 22:45:54 +0530

    Time: 0 ms (100.00%), Space: 44.3 MB (25.95%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.java
@@ -8,9 +8,9 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-        
-        node.val = node.next.val;
-        node.next = node.next.next;
-        
+        //1->2->3->null; node to be deleted = 2.
+        node.val = node.next.val;             //2=3. 1->3->3->null
+        node.next = node.next.next;           //1->3->null. the second node 3 will point to null, skipping the third node 3. Thus, indirectly deleting node 2 from the linked list.
+
     }
 }
