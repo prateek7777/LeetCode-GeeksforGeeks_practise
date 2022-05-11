37f2a791eb5c39f540b7320a03d5e4570852bd46
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-11 15:27:43 +0530

    Time: 14 ms (38.68%), Space: 46.3 MB (25.54%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -6,22 +6,24 @@ class Solution {
         HashMap<Character, Integer> memo = new HashMap<>();
         int n = s.length();
         
-        release = 0;
-        for(int acquire = 0; acquire<n; acquire++){
+        release = 0;                                       //initialising release to 0.
+        for(int acquire = 0; acquire<n; acquire++){        //start iterating from acquire = 0.
             
-            char currChar = s.charAt(acquire);
+            char currChar = s.charAt(acquire);             //the base, or the very first element that has to be started with.
             
-            while(release<acquire && memo.containsKey(currChar)){
-                char discardChar = s.charAt(release);
-                memo.remove(discardChar);
-                release = release + 1;
+            while(release<acquire && memo.containsKey(currChar)){         //releasing the element, in case, it is repeating.
+                char discardChar = s.charAt(release);                 
+                memo.remove(discardChar);                    //removing(discarding) the element present at release pointer.
+                release = release + 1;               //since the element(char) at release has been discarded, release pointer is incremented.
             }
             
-            memo.put(currChar, acquire);
-            answer = Math.max(answer, acquire-release+1);
-            
+            memo.put(currChar, acquire);                       //if the element is not repeating, the char has to be put in the HashMap, with its index assigned as its value.
+            //longest -> memo.put(currChar, index); count -> memo.put(currChar, frequency).
+            answer = Math.max(answer, acquire-release+1);  //the maximum because, the length of the "longest" substring has to be found.
+            //also, the length of the valid substring will be (acqurie-release+1).
         }
         return answer;
+        //keep on acquiring until an invalid case is encountered, keep on releasing until a valid case is encountered.
         
     }
 }
