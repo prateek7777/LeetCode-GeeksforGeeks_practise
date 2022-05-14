38510ba277b59057e3d97cd9ae1d16f8a4897ab7
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-14 14:59:25 +0530

    Time: 51 ms (68.17%), Space: 44.8 MB (89.56%) - LeetHub

--- 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
@@ -22,27 +22,25 @@ class Solution {
 		        memo.put(currInt, 1);                 //not repeating, first time.
 		        distinct = distinct + 1;           //since, first time encounter, increment distinct.
 		    }
-		    
-		                               
+		                             
 		    while(release<=acquire && distinct > k){     //as per the question.
 		        
 		        int discardInt = nums[release];    //required
 		        release = release + 1;                     //incrementing release as it has come inside the loop meaning, that the 
-		        //previous chars should be released until the valid condition is reached, i.e (distinct <= k)(atmost k). 		        
+		        //previous ints should be released until the valid condition is reached, i.e (distinct <= k)(atmost k). 		        
 		        memo.put(discardInt, memo.get(discardInt)-1);  //decrementing the frequency without if loop as while in itself is 
 //a condition that checks the credibility.		        
-		        if(memo.get(discardInt) == 0)     //if frequency of char is 0, it has to be removed from the HashMap.
+		        if(memo.get(discardInt) == 0)     //if frequency of int is 0, it has to be removed from the HashMap.
  		        {
-		            memo.remove(discardInt);      //since we are completely removing a char(not decrementing it).
+		            memo.remove(discardInt);      //since we are completely removing a int(not decrementing it).
 		            distinct = distinct - 1;       //since we are removing(opposite of adding), distinct count should be reduced by 1.
 		        }
 		        
 		    }
         
-		answer = answer + acquire-release+1; //the count of the valid substring(s) is (acquire-release+1). we keep updating the answer.
+		answer = answer + acquire-release+1; //the count of the valid subarray(s) is (acquire-release+1). we keep updating the answer.
 		
 		}
-				return answer;
-        
+				return answer;  //returning the answer for AT MOST k.
     }
 }
