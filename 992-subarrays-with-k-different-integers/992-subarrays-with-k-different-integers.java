788a348af085a12aabde3772931530b8fe2703a7
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-14 14:36:20 +0530

    Time: 93 ms (24.50%), Space: 75.2 MB (6.83%) - LeetHub

---
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return subarrayWithAtmostK(nums, k-1) - subarrayWithAtmostK(nums, k);
+    }
+        public int subarrayWithAtmostK(int[] nums, int k){
+        int release = 0;
+		int answer = 0;
+		int distinct = 0;
+		int n = nums.length;
+        
+		HashMap<Integer, Integer> memo = new HashMap<Integer, Integer>();
+		
+		for(int acquire = 0; acquire<n; acquire++){
+		    
+		    int currInt = nums[acquire];       //required.
+		    
+		    if(memo.containsKey(currInt))            //if repeating.
+		        memo.put(currInt, memo.get(currInt)+1);
+		        
+		    else
+		    {
+		        memo.put(currInt, 1);                 //not repeating, first time.
+		        distinct = distinct + 1;           //since, first time encounter, increment distinct.
+		    }
+		    
+		                               
+		    while(release<=acquire && distinct > k){     //as per the question.
+		        
+		        int discardInt = nums[release];    //required
+		        release = release + 1;                     //incrementing release as it has come inside the loop meaning, that the 
+		        //previous chars should be released until the valid condition is reached, i.e (distinct <= k)(atmost k). 		        
+		        memo.put(discardInt, memo.get(discardInt)-1);  //decrementing the frequency without if loop as while in itself is 
+//a condition that checks the credibility.		        
+		        if(memo.get(discardInt) == 0)     //if frequency of char is 0, it has to be removed from the HashMap.
+ 		        {
+		            memo.remove(discardInt);      //since we are completely removing a char(not decrementing it).
+		            distinct = distinct - 1;       //since we are removing(opposite of adding), distinct count should be reduced by 1.
+		        }
+		        
+		    }
+        
+		answer = answer + acquire-release+1; //the count of the valid substring(s) is (acquire-release+1). we keep updating the answer.
+		
+		}
+				return (n*(n+1)/2) - answer;
+        
+    }
+}
