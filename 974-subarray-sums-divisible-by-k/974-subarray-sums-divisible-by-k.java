ffe741c1b7759ae8cd398b8cf795c3173e8ecd6b
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-15 23:24:45 +0530

    Time: 29 ms (43.28%), Space: 63.8 MB (7.56%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.java
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.java
@@ -5,19 +5,19 @@ class Solution {
         int currRem = 0;
         HashMap<Integer, Integer> memo = new HashMap<>();
         
-        memo.put(currRem, 1);
+        memo.put(currRem, 1);                                 //to handle edge cases.
         for(int currVal : nums){
-        prefixSum = prefixSum + currVal;
-        currRem = ((prefixSum%k)+k)%k;
+        prefixSum = prefixSum + currVal;                      //prefixSum is the sum of the elements before and including the currVal.
+        currRem = ((prefixSum%k)+k)%k;                        //currRem = prefixSum%k; but to handle the negative remainders, as negative remainders cannot exist, they are converted to positive.
             
-        if(memo.containsKey(currRem)){
-            answer = answer + memo.get(currRem);
-            memo.put(currRem, memo.get(currRem)+1);
+        if(memo.containsKey(currRem)){                        //if the currRem is repeating.
+            answer = answer + memo.get(currRem);              //answer has to be updated with the frequency of the HashMap.
+            memo.put(currRem, memo.get(currRem)+1);           //increment the frequency of the HashMap for its use in further iterations.
         }
             else
-            memo.put(currRem, 1);
+            memo.put(currRem, 1);                              //if the currRem has been encountered for the first time.
   
         }
-        return answer;
+        return answer;                                          //we return the final answer.
     }
 }
