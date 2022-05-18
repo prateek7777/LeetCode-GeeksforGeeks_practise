4cf600d554f53ba6b5e6ea12982a29d027196e58
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-18 23:36:17 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (82.22%) - LeetHub

--- 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.java
+++ 559-maximum-depth-of-n-ary-tree/559-maximum-depth-of-n-ary-tree.java
@@ -20,14 +20,16 @@ class Node {
 class Solution {
     public int maxDepth(Node root) {
         
-        if(root == null)
+        if(root == null)                 //this is the base case.
             return 0;
         
-        int f_depth = 0;
-        for(Node currNode : root.children)
-            f_depth = Math.max(f_depth, maxDepth(currNode));
+        int f_depth = 0;                                         //f_depth is the maximum depth/height of this tree.
+        for(Node currNode : root.children)                       //iterating through the given n-arry tree. (root.children) is required, because only "children" cannot be identified by the compiler.
+            f_depth = Math.max(f_depth, maxDepth(currNode));     //we need to return the maximum of all the nodes, which represents the depth of the entire n-ary tree.
+        //we don't do Math.max(f_depth, 1+maxDepth(currNode)) because the for loop won't reach until the null node, as it will stop at the very last node having data. our output will always be 1
+        //less than the required output.
         
-        return 1 + f_depth;
+        return f_depth+1;         //thus, to add 1(the null node) to the count of the depth of the n-ary tree, we add 1 to the f_depth.
     
     }
 }
