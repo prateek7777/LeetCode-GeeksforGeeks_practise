b5340ce2fabce3c54260826847686f7f9c91ee0f
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-20 06:57:49 +0530

    Time: 1 ms (66.15%), Space: 50.6 MB (57.26%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.java
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.java
@@ -20,20 +20,20 @@ class Solution {
     
     public TreeNode merge(TreeNode t1, TreeNode t2){
         
-        if(t1==null && t2==null)
+        if(t1==null && t2==null)             //base case.
             return null;
         
-        if(t1 == null)
+        if(t1 == null)   //in order to add a node when tree t1 has null at the same point, but t2 has a node at the same point.
             return t2;
         
-        if(t2 == null)
+        if(t2 == null)   //in order to add a node when tree t2 has null at the same point, but t1 has a node at the same point.
             return t1;
         
-        TreeNode root3 = new TreeNode(t1.val + t2.val);
-        root3.left = merge(t1.left, t2.left);
-        root3.right = merge(t1.right, t2.right);
-        
-        return root3;
+        TreeNode root3 = new TreeNode(t1.val + t2.val);       //adding the node.
+        root3.left = merge(t1.left, t2.left);                 //adding the left child.
+        root3.right = merge(t1.right, t2.right);              //adding the right child.
+                                                              //thus, preorder traversal.
+        return root3;                                         //returning the root of the final answer(addition of root1 and root2).
     }
     
 }
