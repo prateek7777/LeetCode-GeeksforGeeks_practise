a2d775894fa258bd13c7f349b11e0650299ab708
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-21 19:02:09 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (38.33%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.java
+++ 101-symmetric-tree/101-symmetric-tree.java
@@ -16,21 +16,23 @@
 class Solution {
     public boolean isSymmetric(TreeNode root) {
         return isSymmetricCheck(root, root);
-    }
-    
+    }  
+    //almost same as "Same Tree"(100).
+    //symmetric means, that the left child of the left node and the right child of the right node are same.
+    //similarly, the right child of the left node and the left child of the right node are same.
     public boolean isSymmetricCheck(TreeNode p, TreeNode q){
-        if(p == null && q == null)       //we reached the end of the two trees without encountering any differences.
+        if(p == null && q == null)       //we reached the end of the two nodes without encountering any differences.
         return true;                     //thus, we return true.
         
-        if(p == null || q == null)        //even if one of the nodes is null, it indicates that the two are not equal. 
+        if(p == null || q == null)        //even if one of the nodes is null, it indicates that the two are not symmetric. 
         return false;                     //thus, we return false.
         
-        if(p.val != q.val)                //if the values of p and q are not equal, it indicates that the trees are not same.
-            return false;                 //also, null.val cannot be checked here, hence it is checked in line 22 itself.
+        if(p.val != q.val)                //if the values of p and q are not equal, it indicates that the left and right sides are not symmetric.
+            return false;    //also, null.val cannot be checked here, hence it is checked in line 27 itself.
                                         
-        boolean compareLAndR = isSymmetricCheck(p.left, q.right);           //recursive call on the left side of the 2 trees.
-        boolean compareRAndL = isSymmetricCheck(p.right, q.left);           //recursive call on the right side of the 2 trees.
+        boolean compare1 = isSymmetricCheck(p.left, q.right); //compare left side of p(left child) and right side of q(right child).
+        boolean compare2 = isSymmetricCheck(p.right, q.left); //compare right side of p(left child) and left side of q(right child).
         
-        return compareLAndR && compareRAndL;      //returns true if equal or else, false is returned
+        return compare1 && compare2;      //returns true if symmetric or else, false is returned.
     }
 }
