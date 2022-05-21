3475d0eb82a787e195b6480cfe82e7f68fba247d
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-21 07:21:19 +0530

    Time: 12 ms (5.48%), Space: 42.4 MB (17.21%) - LeetHub

--- 872-leaf-similar-trees/872-leaf-similar-trees.java
+++ 872-leaf-similar-trees/872-leaf-similar-trees.java
@@ -15,26 +15,26 @@
  */
 class Solution {
     public boolean leafSimilar(TreeNode root1, TreeNode root2) {
-        String[] leavesOfTree1 = {""};
+        String[] leavesOfTree1 = {""};                             //String array of size 1 for constant space.
         String[] leavesOfTree2 = {""};
         
-        findLeaves(root1, leavesOfTree1);
-        findLeaves(root2, leavesOfTree2);
+        findLeaves(root1, leavesOfTree1);                          //all leaves of Tree 1.
+        findLeaves(root2, leavesOfTree2);                          //all leaves of Tree 2.
         
-        return leavesOfTree1[0].equals(leavesOfTree2[0]);
+        return leavesOfTree1[0].equals(leavesOfTree2[0]);          //returns true if equal.
     }
     
     public void findLeaves(TreeNode root, String[] allLeaves){
     
-        if(root == null)
+        if(root == null)                                                  //if reached the end.
             return;
         
-        if(root.left == null && root.right == null)
-            allLeaves[0] = allLeaves[0] + Integer.toString(root.val) + "-";
+        if(root.left == null && root.right == null) //if reached the leaf node.
+            allLeaves[0] = allLeaves[0] + Integer.toString(root.val) + "-"; //delimiter is compulsory as not all the testcases get passed withuout it.
+        //12-1-0 and 1-21-0 are not same, but 1210 and 1210 are same.
         
-        findLeaves(root.left, allLeaves);
-        findLeaves(root.right, allLeaves);
+        findLeaves(root.left, allLeaves);              //if not reached the leaf node, find leaves from the left child.
+        findLeaves(root.right, allLeaves);             //if not reached the leaf node, find leaves from the right child.
         return;
     }
-    
 }
