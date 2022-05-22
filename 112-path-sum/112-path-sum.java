9243c69d4c673f0de550a761fabab9ccd7970bdd
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-22 15:24:14 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (23.76%) - LeetHub

--- 112-path-sum/112-path-sum.java
+++ 112-path-sum/112-path-sum.java
@@ -16,15 +16,15 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null)
-            return false;
+            return false;    //traversed through the entire tree and the target sum was not reached.       //base case.
         
-        else if(root.left == null && root.right == null && (targetSum-root.val) == 0)
-            return true;
+        else if(root.left == null && root.right == null && (targetSum-root.val) == 0)  //if the leaf node has been reached and the target sum has been reached.
+            return true;   
         
-        else{
-            boolean leftSubtree = hasPathSum(root.left, targetSum - root.val);
-            boolean rightSubtree = hasPathSum(root.right, targetSum - root.val);
-            
+        else{  //(targetSum - root.left.val) cannot be used, as in case of a leaf node, root.left = null, which throws an error( null.val cannot be computed ).
+            boolean leftSubtree = hasPathSum(root.left, targetSum - root.val);     //keep searching in the left subtree for the target sum until reaching the target sum.
+            boolean rightSubtree = hasPathSum(root.right, targetSum - root.val);   //keep searching in the right subtree for the target sum until reaching the target sum.
+            //either the target sum is reached in the left subtree or the right subtree. until then, we keep searching. hence, the "OR" condition is used instead of "AND" condition.
             return leftSubtree || rightSubtree;
         }
     }
