447c9622c8cb47c083228d8dbc501cac6adc0061
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-23 11:26:25 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (71.33%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -15,20 +15,23 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> pre_traversal = new ArrayList<>();                 //this list contains the preorder traversal list.
-        preTraversal(root, pre_traversal);                               //recursive function call.
-        return pre_traversal;                                            //return the final traversed list.
-    }
-    
-    public void preTraversal(TreeNode root, List<Integer> pre_traversal){
+       List<Integer> answer = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
         
-        if(root == null)                                                    //the base case.
-            return;
-        //preorder traversal => value at node, left node, right node.
-        pre_traversal.add(root.val);             //recursion not needed here. //the value at the current node/root is added to the list.
-        preTraversal(root.left, pre_traversal);        //recursively keep adding the left node(root), to the list.
-        preTraversal(root.right, pre_traversal);       //recursively keep adding the right node(root), to the list.
+        if(root == null)
+            return answer;
         
-        return;                                   //the return type is void.
-    }
-}
+        stack.push(root);
+        
+        while(!stack.isEmpty()){
+         TreeNode currNode = stack.pop();
+            answer.add(currNode.val);
+            
+            if(currNode.right != null)
+                stack.push(currNode.right);
+            
+            if(currNode.left != null)
+                stack.push(currNode.left);
+        }
+    return answer;
+    }}
