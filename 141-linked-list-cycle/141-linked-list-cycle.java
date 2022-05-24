f3865cc1f46783f57102a0999a495356eb22fa7f
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-24 17:03:38 +0530

    Time: 1 ms (35.89%), Space: 45.9 MB (41.83%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -12,23 +12,23 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
      
-        if(head == null)
+        if(head == null)                       //we cannot do null.next in line 19.
             return false;
-        
-        ListNode slow = head;
-        ListNode fast = head.next;
-        
+         //if there is a cycle in the Linked List, then slow pointer and fast pointer will meet each other.
+        ListNode slow = head;      
+        ListNode fast = head.next; //slow pointer and fast pointer don't start at the same point.             
+        //the fast pointer will simply loop back into the list if there is a cycle in the Linked List.
         while(slow != fast){
             
-            if(fast == null || fast.next == null){
-                return false;
+            if(fast == null || fast.next == null){     //if fast pointer reaches this point, there is no cycle in the given Linked List.
+                return false;                          //thus, we return false.
         }
-            slow = slow.next;
-            fast = fast.next.next;            
+            slow = slow.next;                           
+            fast = fast.next.next;                     //iterating slow pointer and fast pointer according to their speeds.
             
         }
         
-        return true;
-        
+        return true;              //if the loop breaks, it indicates that there is a cycle in the Linked List, because of which slow and fast got equal.
+                                 
     }
 }
