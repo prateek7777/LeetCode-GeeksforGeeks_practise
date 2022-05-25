44966e2891b80b938a517d4adcdf3673e6c1bb1c
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-25 14:10:38 +0530

    Time: 1 ms (41.96%), Space: 41.4 MB (69.66%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -15,28 +15,28 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        
+        //inorder is LEFT to RIGHT, with root in between.
         Stack<TreeNode> stack = new Stack<>();
         List<Integer> output = new ArrayList<>();
         
-        if(root == null)
+        if(root == null)                                            //base case.
             return output;
         
-        TreeNode curr = root;
+        TreeNode curr = root;                                       //for the iteration.
         
-        while(!(curr == null) || !stack.isEmpty()){
+        while(!(curr == null) || !stack.isEmpty()){                 //for the 1st time, stack is empty, but curr is not null.
             
-            while(!(curr == null)){
-                stack.push(curr);
-                curr = curr.left;
+            while(!(curr == null)){  //LEFT.  
+                stack.push(curr);    //push FIRST because if curr becomes null, the very last element won't be pushed into the stack.
+                curr = curr.left;    //iterating through the left subtree.
             }
             
-            curr = stack.pop();
-            output.add(curr.val);
-            curr = curr.right;
-        }
+            curr = stack.pop();      //as soon as the iteration through the left subtree is over, we start popping the elements and put them in our output.
+            output.add(curr.val);    //populating the output.
+            curr = curr.right;       //RIGHT. //iterating through the right subtree.
+        }                            //as soon as the stack becomes empty, we come out of the while loop.
         
-        return output;
+        return output;               //returning the final output.
         
     }
 }
