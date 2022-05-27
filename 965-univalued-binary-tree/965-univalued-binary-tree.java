0c9b8a05c57a2c849e4c0419c068209bedef7d4f
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-27 13:30:49 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (40.12%) - LeetHub

--- 965-univalued-binary-tree/965-univalued-binary-tree.java
+++ 965-univalued-binary-tree/965-univalued-binary-tree.java
@@ -15,23 +15,28 @@
  */
 class Solution {
     public boolean isUnivalTree(TreeNode root) {
-        return isUnivalued(root, root.val);
-    }
-    
-    public boolean isUnivalued(TreeNode root, int considerVal){
-     
-        if(root == null)   //we return true, because null at root, implies that we have reached the null of every node in the tree without contradicting the requirement.
-            return true;  
+        Queue<TreeNode> q = new LinkedList<>();
         
-        if(root.val != considerVal)                //root.val keeps on changing with each node, while considerVal only contains the value at the original root of the given tree.
-            return false;                          //inequality implies that the given binary tree is not univalued.
-         
-        boolean leftConsider = isUnivalued(root.left, considerVal);    //consider left child.     
-        boolean rightConsider = isUnivalued(root.right, considerVal);  //consider right child.
+        int considerVal = root.val;
         
-        if(leftConsider && rightConsider == true)                    //comparing the left child and right child.
-            return true;                                             
-        else
-            return false;
+        q.add(root);                                             //add something.
+        //start BFS.
+        while(!q.isEmpty()){
+
+                TreeNode currNode = q.remove();                  //remove.
+                
+                if(currNode.val != considerVal)                 //do something according to the question.
+                    return false;
+            
+                if(currNode.left != null)                        //process children.
+                    q.add(currNode.left);
+                
+                if(currNode.right != null)
+                    q.add(currNode.right);
+                    
+            }
+
+        return true;                                  //if we come out of the loop without returning false, the tree is Univalued.
+
     }
 }
