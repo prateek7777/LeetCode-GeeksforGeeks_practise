542242c3978414cd64d1261f76727635b17d33a7
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-28 14:08:12 +0530

    Time: 1 ms (58.15%), Space: 41.6 MB (63.74%) - LeetHub

--- 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.java
@@ -13,37 +13,58 @@
  *     }
  * }
  */
-class Solution {
-    public boolean isCousins(TreeNode root, int x, int y) {
-        int[] parents = new int[2];          //storing the parents
-        int[] levels = new int[2];           //storing the levels
-        //TreeNode(-1) is a dummy node created as we cannot find null.val in line 34.
-        areCousins(root, new TreeNode(-1), 0, x, y, parents, levels);
-        //we start at (given root, parent of given root, current Level, x, y, parents, levels).
-        return (parents[0] != parents[1]) && (levels[0] == levels[1]); //the condition to be cousins. different parents at the same level.
-    }
-    
-    public void areCousins(TreeNode root, TreeNode currParent, int currLevel, int x, int y, 
-                           int[] parents, int[] levels){
-        if(root == null)                //if the root is null, we just return.
-            return;
-        //we want to stop as soon as parents[0] and parents[1] are populated, indicating the required information has been gained through recursion.
-        if(parents[0] != 0 && parents[1] != 0)
-            return;
-        
-        if(x == root.val)               //if the root is equal to one of the given cousins(can be true or false)(x)
-        { //we use index 0 for storing the information of x.
-            parents[0] = currParent.val;              //we cannot put currParent directly into parents[0] as currParent is a node.
-            levels[0] = currLevel;
-        }
-        
-        if(y == root.val)               //for y cousin.
-        { //we use index 1 for storing the information of y.
-            parents[1] = currParent.val;
-            levels[1] = currLevel;
-        }
-        //the two required function calls for progressing towards left and right.
-        areCousins(root.left, root, currLevel+1, x, y, parents, levels);  //root.left is going ahead in left direction. root is the parent of root.left, currLevel is incrementing as well.
-        areCousins(root.right, root, currLevel+1, x, y, parents, levels); //root.right is going ahead in right direction. root is the parent of root.right, currLevel is incrementing as well.
+class Pair{
+    TreeNode node;
+    TreeNode parent;
+    Pair(TreeNode node, TreeNode parent){
+        this.node = node;
+        this.parent = parent;
     }
 }
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        Queue<Pair> q = new LinkedList<>();
+        TreeNode parentOfx = null;
+        TreeNode parentOfy = null;
+        
+        q.add(new Pair(root, new TreeNode(-1)));
+        
+        while(!q.isEmpty()){        
+        int currLevelSize = q.size();
+        
+        for(int i = 0;i<currLevelSize; i++){
+            
+            Pair currPair = q.remove();
+            TreeNode currNode = currPair.node;
+            TreeNode currParent = currPair.parent;
+            
+            if(currNode.val == x)
+                 parentOfx = currParent; 
+            
+            if(currNode.val == y)
+                parentOfy = currParent;
+            
+            if(currNode.left != null)
+                q.add(new Pair(currNode.left, currNode));
+            
+            if(currNode.right != null)
+                q.add(new Pair(currNode.right, currNode));
+                      
+                      }
+            
+            if(parentOfx != null && parentOfy != null){
+                if(parentOfx.val == parentOfy.val)
+                    return false;
+                else
+                    return true;
+            }
+            
+            if(parentOfx != null || parentOfy != null)
+                return false;
+                      
+        }
+        
+        return false;
+
+        }
+    }
