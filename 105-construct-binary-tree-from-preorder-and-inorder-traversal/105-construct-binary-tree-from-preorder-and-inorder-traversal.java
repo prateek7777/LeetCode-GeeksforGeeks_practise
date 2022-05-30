1c1c2abf729f80c24a26b377592e226da1eb55c0
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-05-30 14:06:58 +0530

    Time: 4 ms (54.58%), Space: 44.7 MB (32.75%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -15,33 +15,33 @@
  */
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-       int[] preIndex = {0};
-       HashMap<Integer, Integer> inorderMap = buildInorderMap(inorder);
-       return constructTree(preorder, inorder, preIndex, 0, preorder.length-1, inorderMap); 
+       int[] preIndex = {0}; //an integer value will not get updated with all the iterations. because it is pass by value. while the int[] value will get updated because it is pass by reference.
+       HashMap<Integer, Integer> inorderMap = buildInorderMap(inorder);          //the HashMap.
+       return constructTree(preorder, inorder, preIndex, 0, preorder.length-1, inorderMap);  //the function call.
     }
     
     public TreeNode constructTree(int[] preorder, int[] inorder, int[] preIndex, int inStart, int inEnd, HashMap<Integer, Integer> inorderMap){
         
-        if(preIndex[0] >= preorder.length || inStart > inEnd)
+        if(preIndex[0] >= preorder.length || inStart > inEnd)       //no elements will be there to process. hence, return null.
             return null;
         
-        int currVal = preorder[preIndex[0]];
-        TreeNode currNode = new TreeNode(currVal);
-        preIndex[0] = preIndex[0] + 1;
+        int currVal = preorder[preIndex[0]];             //the element at the preorder array(used for the nodes iteration).
+        TreeNode currNode = new TreeNode(currVal);       //currNode at the currVal.
+        preIndex[0] = preIndex[0] + 1;                   //iterate preIndex[0] with each call, use it as a pointer in the preorder array.
         
-        int i = inorderMap.get(currVal);
-        
-        currNode.left = constructTree(preorder, inorder, preIndex, inStart, i-1, inorderMap);
-        currNode.right = constructTree(preorder, inorder, preIndex, i+1, inEnd, inorderMap);
+        int i = inorderMap.get(currVal);                 //get the index of the place in the inorder array, where the currVal is located.
+        //the inorder array has been used to find the left and the right subtree of the given tree.
+        currNode.left = constructTree(preorder, inorder, preIndex, inStart, i-1, inorderMap);      //iterating through the left subtree.
+        currNode.right = constructTree(preorder, inorder, preIndex, i+1, inEnd, inorderMap);       //iterating through the right subtree.
 
-        return currNode;
+        return currNode;                              //returning the node of the built tree.
     }
     
     public HashMap<Integer, Integer> buildInorderMap(int[] arr){   
         HashMap<Integer, Integer> memo = new HashMap<>();
-        
+        //the function to assign the index to each value in the inorder array. this is used above in the function call for finding the index of the currVal in the inorder array.
         for(int i = 0; i<arr.length; i++){
-            memo.put(arr[i], i);
+            memo.put(arr[i], i); 
         }
         return memo;
     }
