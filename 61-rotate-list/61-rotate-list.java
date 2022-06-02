697d86e8fc022ae729ceeceac89e1282298f664b
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-06-02 22:15:48 +0530

    Time: 1 ms (69.74%), Space: 42.4 MB (69.10%) - LeetHub

--- 61-rotate-list/61-rotate-list.java
+++ 61-rotate-list/61-rotate-list.java
@@ -10,22 +10,23 @@
  */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null || head.next == null || k == 0)
+        if(head == null || head.next == null || k == 0)                  //edge cases.
             return head;
         
         ListNode curr = head;
         int len = 0;
         
-        len = 1;
-        while(curr.next != null)
+        len = 1; 
+        while(curr.next != null)                                  //calculating the length of LinkedList.
         {
             curr = curr.next;
             len = len + 1;
-        }
+        }  
         
-        curr.next = head;
-        k = k%len;
-        k = len - k;
+        curr.next = head;                                        //converting the linear LinkedList into circular LinkedList.
+        k = k%len;                                               //k = 12, len = 5; k = 12%5 = 2. rotating 2 times is enough.
+        //a simple dry run from starting of the code is enough.
+        k = len - k;                                             
         while(k > 0){
             curr = curr.next;
             k = k - 1;
