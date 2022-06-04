01efd7c3985331a5b97fe97e2c3ddf0213b78c1d
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-06-05 00:23:24 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (90.23%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -4,18 +4,18 @@ class Solution {
         int temp = 0;
         int answer = 0;
         
-        for(int i : nums){
+        for(int i : nums){                //iterating through the array.
             
-            if(memo.containsKey(i)){
-                temp = memo.get(i);
-                answer = answer + temp;
-                memo.put(i, temp+1);
+            if(memo.containsKey(i)){      //if the number has repeated.
+                temp = memo.get(i);       //get the count(occurences) of that number.
+                answer = answer + temp;   //add the number of occurences of that number to the total number of occurences.
+                memo.put(i, temp+1);      //increment the occurence of that number by 1. 
             }
             else
-                memo.put(i, 1);
+                memo.put(i, 1);           //if the number hasn't been encountered before, then it's occurence is set to 1.
             
         }
 
-        return answer;
+        return answer;                    //returning the final answer.
     }
 }
