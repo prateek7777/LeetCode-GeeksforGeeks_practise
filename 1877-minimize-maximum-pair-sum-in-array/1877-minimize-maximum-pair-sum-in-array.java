dc6964f914743bfc3afcc6bc079a7e00ef6572f5
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-06-08 14:24:13 +0530

    Time: 67 ms (49.39%), Space: 105.7 MB (50.50%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.java
@@ -4,12 +4,12 @@ class Solution {
         int len = nums.length;
         int res = Integer.MIN_VALUE;
         
-        Arrays.sort(nums);
+        Arrays.sort(nums);                //sorting in ascending order.
         
-        for(int i = 0; i<len; i++){
-            res = Math.max(res, nums[i]+nums[len - i - 1]); 
-        }
-        
+        for(int i = 0; i<len/2; i++){     //iterating till the first half is enough.
+            res = Math.max(res, nums[i]+nums[len - i - 1]); //the first half will be half-small, the second half will be half-large.
+        }                                 //we will be adding the one element from the first half and another element from the second half, from each direction.
+                                          //(left to right (+) right to left).
         return res;
         
     }
