1d5fc65d79d40fa35ce14a8217dea49dff21720e
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-06-15 01:06:26 +0530

    Time: 0 ms (100.00%), Space: 49.7 MB (46.97%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -15,48 +15,48 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null)
+        if(root == null)                 //edge case.
             return null;
         
-        if(key > root.val)
+        if(key > root.val)               //acc. to BST.
             root.right = deleteNode(root.right, key);
-        
-        else if(key < root.val)
+ 
+        else if(key < root.val)          //acc. to BST.
             root.left = deleteNode(root.left, key);
         
-        else{
+        else{   //key==(root.val)                         //found the node having the value key(targetNode is root).
             
-            if(root.left == null && root.right == null){
-                root = null;
+            if(root.left == null && root.right == null){    //no child(null child).
+                root = null;                                //root is targetNode.
                 return root;
             }
             
-            if(root.left != null && root.right == null){
-                root = root.left;
+            if(root.left != null && root.right == null){     //only left child.
+                root = root.left;                            //root is targetNode.
                 return root;
             }
             
-            if(root.left == null && root.right != null){
-                root = root.right;
+            if(root.left == null && root.right != null){     //only right child.
+                root = root.right;                           //root is targetNode.
                 return root;
             }
-            
-            TreeNode subsNode = findNextMax(root.right);
+            //both left and right childs exist for the targetNode.
+            TreeNode subsNode = findNextMax(root.right);      //the next max node(the very next of targetNode). 
             
             int temp = 0;
-            temp = root.val;
+            temp = root.val;                 //swapping targetNode val and the subsNode val.
             root.val = subsNode.val;
             subsNode.val = temp;
-            
-            root.right = deleteNode(root.right, key);
-            
+            //right subtree, with minimum value on the left side is the next max node(inorder intuition).
+            root.right = deleteNode(root.right, key);  //the node to be deleted(targetNode is on right side.) is deleted as it takes the place of the subsNode.(right subtree, extreme left).
+                                                       
         }
              return root;
     }
     
     public TreeNode findNextMax(TreeNode current){
                     
-           while(current.left != null)
+           while(current.left != null)                  //going to the extreme left of the right subtree(for the subsNode).
                current = current.left;
         
         return current;
