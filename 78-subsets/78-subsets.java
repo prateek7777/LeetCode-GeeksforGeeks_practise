26ad94999badad8ca5242c69bddf7bc6aa8c57a0
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-06-19 19:12:16 +0530

    Time: 1 ms (87.15%), Space: 43.1 MB (69.16%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -7,17 +7,18 @@ class Solution {
     
     public void generateSubsets(int[] nums, int currIndex, List<Integer> currSubset, List<List<Integer>> allSubsets){
         
-        if(currIndex >= nums.length){
-            allSubsets.add(new ArrayList<>(currSubset));
-            return;
+        if(currIndex >= nums.length){ //base case. (it runs multiple times for every end of the recursive call).
+            allSubsets.add(new ArrayList<>(currSubset)); //allSubsets.add(currSubset) will lead to allSubsets = {[],[],[],[]}, because the same memory location will keep getting updated for every
+            return; //recursive call. it will be empty because of lines 15 and 17. adding the new keyword will allocate a different memory location to currSubset for every recursive call.
         }
         
-        currSubset.add(nums[currIndex]);
-        generateSubsets(nums, currIndex+1, currSubset, allSubsets);
-        currSubset.remove(currSubset.size()-1);
-        
-        
-        generateSubsets(nums, currIndex+1, currSubset, allSubsets);
-    }
+        currSubset.add(nums[currIndex]);  //adding the number to the current subset.
+        generateSubsets(nums, currIndex+1, currSubset, allSubsets); //the "consider array number" function call.
+        currSubset.remove(currSubset.size()-1); //removing the number to avoid an output like: allSubsets = {[1,2,2], [1,2,2], [1,2,2], [1,2,2]}.
+        //there will be no difference in the consider and the not consider function call.
     
+        generateSubsets(nums, currIndex+1, currSubset, allSubsets);  //the "not consider array number" function call.
+        //the consider and the not consider function call will be different because of the contains of currSubset list, which is because of lines 15 and 17.
+        
+    }
 }
