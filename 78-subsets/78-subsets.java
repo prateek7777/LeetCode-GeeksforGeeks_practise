1c27f9f63f88ce01b67047ad4f6410206628fc7b
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-06-19 17:54:26 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (93.11%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,18 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        // Arrays.sort(nums);                                    //sorting the array, so that, the duplicate elements are adjacent to each other. Not required in this question.
-        List<List<Integer>> answerList = new ArrayList<>();                       //The syntax of ArrayList should be revised at each and every point. 
-        findSubsets(0, nums, new ArrayList<>(), answerList);                      //the recursive function call.
-        return answerList;                                       //returning the final answer.
+        List<List<Integer>> allSubsets = new ArrayList<>();
+        generateSubsets(nums, 0, new ArrayList<>(), allSubsets);
+        return allSubsets;
     }
     
-    public static void findSubsets(int start, int[] nums, List<Integer> individualSubset, List<List<Integer>> answerList){
+    public void generateSubsets(int[] nums, int currIndex, List<Integer> currSubset, List<List<Integer>> allSubsets){
         
-        answerList.add(new ArrayList<>(individualSubset));         //for the very first empty list([]) to be added in the final answer list.
-        for(int i = start; i<nums.length; i++){                    //start = 0.
-            individualSubset.add(nums[i]);                         //adding the number from the array to the individual subset(list).
-            findSubsets(i+1, nums, individualSubset, answerList);  //starting at (i+1), because, the number at (i) has been already added in line 13.
-            individualSubset.remove(individualSubset.size()-1);    //the size of the individual subset will reach upto (=nums.length) at its maximum size. However, the for loop is running only from
-        }                                                          //0 to (nums.length-1). Thus, the iteration will stop as it will come out of for loop. To avoid that, we do line 15.
+        if(currIndex >= nums.length){
+            allSubsets.add(new ArrayList<>(currSubset));
+            return;
+        }
+        
+        currSubset.add(nums[currIndex]);
+        generateSubsets(nums, currIndex+1, currSubset, allSubsets);
+        currSubset.remove(currSubset.size()-1);
+        
+        
+        generateSubsets(nums, currIndex+1, currSubset, allSubsets);
     }
+    
 }
