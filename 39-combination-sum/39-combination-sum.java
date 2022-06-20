6ee82feae0fcdc2023842eb0be6e455c9899451d
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-06-21 01:36:54 +0530

    Time: 3 ms (94.26%), Space: 45 MB (54.16%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,32 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> combinations = new ArrayList<>();
-        findReqdCombinations(0, candidates, target, combinations, new ArrayList<>());
-        return combinations;
+        List<List<Integer>> output = new ArrayList<>();
+        generateCombinations(candidates, target, 0, new ArrayList<>(), output);
+        return output;
     }
     
-    public void findReqdCombinations(int index, int[] candidates, int target, List<List<Integer>> combinations, List<Integer> ds)
-    {
-        if(index == candidates.length)
-        {
-            if(target == 0)
-        {
-            combinations.add(new ArrayList<>(ds));
-        }
+    public void generateCombinations(int[] candidates, int target, int currIndex, List<Integer> curr, List<List<Integer>> output){
+        
+        if(target == 0){
+            output.add(new ArrayList<>(curr));       //new keyword is used to avoid output = [[],[]].
             return;
         }
         
-        if(candidates[index] <= target){
-            
-        ds.add(candidates[index]);
-        findReqdCombinations(index, candidates, target-candidates[index], combinations, ds);
-        ds.remove(ds.size()-1);
-            
+        if(currIndex >= candidates.length)           //edge case.
+            return;
+        
+        if(candidates[currIndex] <= target){         //consider case.
+            curr.add(candidates[currIndex]);         //adding the number to the current subset.
+            generateCombinations(candidates, target-candidates[currIndex], currIndex, curr, output);   //consider funtion call.
+            curr.remove(curr.size()-1);    //to avoid an output like : [[1,1,1],[1,1,1,2]].
         }
-        findReqdCombinations(index+1,candidates, target, combinations, ds);
-
+        
+        generateCombinations(candidates, target, currIndex+1, curr, output);       //not consider case.
     }
     
 }
-
-//Learn BackTracking and come back.
