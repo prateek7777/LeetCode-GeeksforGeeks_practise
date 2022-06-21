0b36e08ba4ad4ed733522cd6bd43834ea3f36273
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-06-22 02:18:33 +0530

    Time: 1 ms (77.20%), Space: 39.4 MB (99.98%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -23,20 +23,20 @@ class Solution {
             
             if(curr != null){
                 
-                stack.push(curr);
-                curr = curr.left;
-                
+                stack.push(curr);                       //just like recursion.
+                curr = curr.left;                       //the smallest element will be on the left side.
+                                                        //starting with the left subtree of the BST.
             }
-            else{
+            else{                                      //(curr == null) indicates that the leftmost node of the subtree has been reached.
                 
-                count = count + 1;
-                TreeNode ele = stack.pop();
+                count = count + 1;                     //??
+                TreeNode ele = stack.pop();             
                 if(count == k)
                     return ele.val;
 
-                curr = ele.right;
+                curr = ele.right;                      //the next subtree on the left subtree of the BST.
             }   
         }
-        return Integer.MAX_VALUE;
+        return Integer.MAX_VALUE;                      
     }
 }
