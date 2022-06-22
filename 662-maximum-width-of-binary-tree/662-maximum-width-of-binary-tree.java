e9fad7d37889bfd3d35acee1f44174919c323b6b
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-06-23 01:45:09 +0530

    Time: 2 ms (84.22%), Space: 43.9 MB (37.74%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -13,49 +13,49 @@
  *     }
  * }
  */
-class Pair{
+class Pair{                            //class.
     TreeNode node;
     int num;
-    Pair(TreeNode node, int num){
-        this.node = node;
+    Pair(TreeNode node, int num){      //constructor.
+        this.node = node;              //initialization.  
         this.num = num;
     }
 }
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
  
-    if(root == null)
+    if(root == null)                    //edge case.
         return 0;
         
-    int ans = 0;    
-    Queue<Pair> que = new LinkedList<>();
-    
-    que.add(new Pair(root, 0));
-    
+    int ans = Integer.MIN_VALUE;                        
+    Queue<Pair> que = new LinkedList<>();    
+     
+    que.add(new Pair(root, 0));         //add something.
+    //start BFS.
     while(!que.isEmpty()){
         int currLevelSize = que.size();
         int first = 0;
         int last = 0;
-        int nMin = que.peek().num;
-        for(int i = 0; i<currLevelSize; i++)
+        int nMin = que.peek().num;            //the minimum value(index) present at each level of the binary tree. peek() brings out top element and queue is FIFO.
+        for(int i = 0; i<currLevelSize; i++)    //iterating through the level from left to right.
         {
-          int currIndex = que.peek().num - nMin;
-            
-          if(i == 0)
-              first = currIndex;
+          Pair currPair = que.remove();         //remove something.
+          TreeNode currNode = currPair.node;
+          int currIndex = currPair.num;
+          currIndex = currIndex - nMin;         //to avoid overflow.
+                                                //do something.
+          if(i == 0)        
+              first = currIndex;                //first node in the level(from left)
           if(i == currLevelSize-1)
-              last = currIndex;
+              last = currIndex;                 //last node in the level(from left)
             
-          TreeNode currNode = que.peek().node;
-          que.poll();
-        
-          if(currNode.left != null)
-              que.add(new Pair(currNode.left, currIndex*2+1));
+          if(currNode.left != null)             //process children.
+              que.add(new Pair(currNode.left, currIndex*2+1));      //as per the 0-indexed calculation, the index of the left node.
           if(currNode.right != null)
-              que.add(new Pair(currNode.right, currIndex*2+2));
+              que.add(new Pair(currNode.right, currIndex*2+2));     //as per the 0-indexed calculation, the index of the right node.
         }
         
-        ans = Math.max(ans, last-first+1);
+        ans = Math.max(ans, last-first+1);    //(last-first+1) denotes the width of the current level of the binary tree.
     }    
    
     return ans;
