c1e4db2a785af8b06d719214f499668cc06458d8
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-06-26 13:50:02 +0530

    Time: 3 ms (5.53%), Space: 43.2 MB (12.58%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -15,7 +15,7 @@
  */
 class Solution {
     public void flatten(TreeNode root) {
-        if(root == null)
+        if(root == null)                        //edge case.
             return;
         
         Stack<TreeNode> stack = new Stack<>();
@@ -24,20 +24,20 @@ class Solution {
         
         while(!stack.isEmpty()){
             
-            TreeNode curr = stack.peek();
+            TreeNode curr = stack.peek();       //starting with curr = root.
             
             stack.pop();
-            
+            //right left root.
             if(curr.right != null)
-                stack.push(curr.right);
+                stack.push(curr.right);         //right   
             
             if(curr.left != null)
-                stack.push(curr.left);
+                stack.push(curr.left);          //left
             
             if(!stack.isEmpty())
-                curr.right = stack.peek();
+                curr.right = stack.peek();      //right->left, because LIFO. here, the last in was left, hence we are pointing the right to the top of the stack.
             
-            curr.left = null;
+            curr.left = null;                   //the left will be always be null after the tree is converted to linkedlist.
             
         }
     }
