5daf8aa57201dc64de2a55bb4a3bc499fda26f84
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-07-02 03:35:58 +0530

    Time: 16 ms (43.85%), Space: 77.3 MB (38.98%) - LeetHub

--- 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
+++ 1373-maximum-sum-bst-in-binary-tree/1373-maximum-sum-bst-in-binary-tree.java
@@ -16,7 +16,7 @@
 class Solution {
 
     public int maxSumBST(TreeNode root) {
-     int[] maxSum = {0};
+     int[] maxSum = new int[1];                  //for pass by reference.
      maxSumBSTHelper(root, maxSum);
      return maxSum[0];
     }
@@ -24,26 +24,26 @@ class Solution {
     private int[] maxSumBSTHelper(TreeNode root, int[] maxSum){
         
         if(root == null)
-            return new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, 0};    //(min, max, size)
+            return new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, 0}; //Initialize {min, max, size}(0,1,2) with {Integer.MAX_VALUE, Integer.MIN_VALUE, size} for the null node.
         
-        int[] leftSubTree = maxSumBSTHelper(root.left, maxSum);
-        int[] rightSubTree = maxSumBSTHelper(root.right, maxSum);  
+        int[] leftSubTree = maxSumBSTHelper(root.left, maxSum);      //left call.
+        int[] rightSubTree = maxSumBSTHelper(root.right, maxSum);    //right call.
         
         if(!(
-                leftSubTree != null &&
-                rightSubTree != null &&
-                root.val > leftSubTree[1] &&
-                root.val < rightSubTree[0]
+                leftSubTree != null &&         //the leftSubTree must be BST.
+                rightSubTree != null &&        //the rightSubTree must be BST.
+                root.val > leftSubTree[1] &&   //the root.val should be greater than the maximum value present in the leftSubTree as per BST.
+                rightSubTree[0] > root.val     //the minimum value present in the rightSubTree should be greater than the root.val as per BST.
             ))
-            return null;
+            return null;                       //if the above conditions are not satisfied, return null.(else go ahead in the function.)
         
-         int sum = leftSubTree[2] + rightSubTree[2] + root.val;
-         maxSum[0] = Math.max(sum, maxSum[0]);
+         int sum = leftSubTree[2] + rightSubTree[2] + root.val;    //the sum is the addition of the size of leftSubTree, rightSubTree and (root.val).
+         maxSum[0] = Math.max(sum, maxSum[0]);                     //as per the problem, calculating the maximum sum.
         
-         int min = Math.min(root.val, leftSubTree[0]);
-         int max = Math.max(root.val, rightSubTree[1]);
+         int min = Math.min(root.val, leftSubTree[0]);    //as per BST, the minimal will be present on the left of the root.
+         int max = Math.max(root.val, rightSubTree[1]);   //as per BST, the maximal will be present on the right of the root.
         
-         return new int[]{min, max, sum};
+         return new int[]{min, max, sum};               //as per the initialization.
             
     }
 
