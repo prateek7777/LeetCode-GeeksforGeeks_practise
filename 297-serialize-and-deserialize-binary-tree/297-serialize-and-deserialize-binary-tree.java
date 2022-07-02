ad4093ee4c6087079fe8b9e5bd45cd54d500e48b
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-07-03 01:17:20 +0530

    Time: 26 ms (46.86%), Space: 53.6 MB (41.93%) - LeetHub

--- 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
+++ 297-serialize-and-deserialize-binary-tree/297-serialize-and-deserialize-binary-tree.java
@@ -8,72 +8,73 @@
  * }
  */
 public class Codec {
-
+    
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        
-        if(root == null)
+            //Serialize: tree to string.
+            //Deserialize: string to tree.
+        if(root == null)   //edge case.
             return "";
         
         Queue<TreeNode> q = new LinkedList<>();
-        StringBuilder result = new StringBuilder();
+        StringBuilder result = new StringBuilder();    //StringBuilder is mutable.
         
-        q.add(root);
-        
+        q.add(root);   //add something.
+        //start BFS. (Level Order Traversal).
         while(!q.isEmpty()){
             
-            TreeNode node = q.remove();
+            TreeNode node = q.remove();           //remove something.
             
-            if(node == null){
+            if(node == null){                     //do something.
                 result.append("n ");
-                continue;
+                continue;              //continue until null node is getting encountered.
             }
             
-            result.append(node.val + " ");
+            result.append(node.val + " ");   //if the node is not null.
             
-            q.add(node.left);
+            q.add(node.left);               //process children.
             q.add(node.right);
   
         }
-        return result.toString();
+        return result.toString();            //return type is string.
     }
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        
-         if(data == "")
+         
+         if(data == "")              //edge case.
              return null;
         
-         Queue<TreeNode> q = new LinkedList<>();
-         String[] parts = data.split(" ");
+         Queue<TreeNode> q = new LinkedList<>();     
+         String[] parts = data.split(" ");          //don't create a new string array.
         
-          TreeNode root = new TreeNode(Integer.parseInt(parts[0]));
-          q.add(root);
-          
+          TreeNode root = new TreeNode(Integer.parseInt(parts[0]));   //the first element of the string array will be the root.
+          q.add(root); 
+          //level order traversal.
           while(!q.isEmpty()){
-              for(int i = 1; i<parts.length; i++){
+              for(int i = 1; i<parts.length; i++){      //start with 1 as the root has already been assigned with parts[0].
                   
                 TreeNode curr = q.remove();
                     
-                if(!(parts[i].equals("n"))){
+                if(!(parts[i].equals("n"))){            //if the element is not null.
                     
                     TreeNode left = new TreeNode(Integer.parseInt(parts[i]));
                     curr.left = left;
-                    q.add(curr.left);
+                    q.add(curr.left);                    //for the next iteration.
                     
                 }  
-                  
-                  if(!(parts[++i].equals("n"))){
+                  //if the element is null, then just leave it as is, it will be null in the tree as well.
+                  if(!(parts[++i].equals("n"))){        //pre-increment for the next iteration.(right).
                   
                     TreeNode right = new TreeNode(Integer.parseInt(parts[i]));
                     curr.right = right;
-                    q.add(curr.right);
+                    q.add(curr.right);                   //for the next iteration.
               }
                   
           }
           }
         
-    return root;
+    return root;       //as per the problem.
     }
 }
 
