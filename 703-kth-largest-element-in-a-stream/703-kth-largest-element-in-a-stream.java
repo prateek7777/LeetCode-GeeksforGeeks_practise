61ebb4a3e5e15576d7736fa60d234aca918315bd
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-07-03 16:50:22 +0530

    Time: 13 ms (97.18%), Space: 46.4 MB (92.36%) - LeetHub

--- 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
+++ 703-kth-largest-element-in-a-stream/703-kth-largest-element-in-a-stream.java
@@ -1,25 +1,25 @@
 class KthLargest {
 
-    private final PriorityQueue<Integer> minHeap;
+    private final PriorityQueue<Integer> minHeap;       //minHeap.
     private final Integer k;
     
     
     public KthLargest(int k, int[] nums) {
         
-        this.k = k;
-        minHeap = new PriorityQueue<Integer>();
-        for(int ele : nums)
+        this.k = k;                                  //initialization.
+        minHeap = new PriorityQueue<Integer>();      //intialization.
+        for(int ele : nums)                          //adding the elements in the minHeap using a for loop and a function call, as we are adding the elements, each at once.
             add(ele);
         
     }
     
     public int add(int val) {
-        
-        minHeap.add(val);
-        if(minHeap.size() > k)
+        //the pivot of the minHeap always points to the smallest element of the minHeap as an implicit property of minHeaps.
+        minHeap.add(val);              //adding to minHeap.
+        if(minHeap.size() > k) //if the minHeap size is greater than k, one element is evicted.as a property of the minHeap, smallest element will be removed.
             minHeap.remove();
         
-        return minHeap.peek();
+        return minHeap.peek(); //the smallest element(the kth largest, naturally) will be at the top of the minHeap(the implicit property) and is just returned, not removed.
         
     }
 }
