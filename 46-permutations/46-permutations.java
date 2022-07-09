a68403beb72c66dca5fed97ab7045eb8e26195ea
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-07-10 04:45:48 +0530

    Time: 3 ms (45.24%), Space: 44.5 MB (61.28%) - LeetHub

---
+++ 46-permutations/46-permutations.java
@@ -0,0 +1,25 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> answer = new ArrayList<>();
+        createPermutations(nums, answer, new ArrayList<>());    //function call.
+        return answer;
+    }
+    
+    public void createPermutations(int[] nums, List<List<Integer>> answer, List<Integer> tempList){
+     
+        if(tempList.size() == nums.length){        //base case.
+            answer.add(new ArrayList<>(tempList)); //adding the sub answer to the final answer with the new keyword to avoid empty list outputs. the "new" keyword leads to allocation of new memory.
+        }
+        else
+        { 
+        for(int i=0; i<nums.length; i++){       //iterating from 0 to nums array (length-1).
+            if(tempList.contains(nums[i]))        //to avoid duplicate elements.
+                continue;
+            tempList.add(nums[i]);               //choose. //update the sub answer.
+            createPermutations(nums, answer, tempList);   //explore. //after updating the sub answer, prepare the function call for the next iteration.
+            tempList.remove(tempList.size()-1);  //unchoose.  //while backtracking, we naturally remove the latest element that was added as per the function calls. because we are going back.
+        }
+        }
+    
+    }
+}
