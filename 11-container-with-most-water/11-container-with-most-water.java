62da14cefcebb2d7c97151fa26bfe4afe94d4e0e
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-07-12 02:38:30 +0530

    Time: 2 ms (99.89%), Space: 75.1 MB (51.47%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,22 +1,19 @@
 class Solution {
     public int maxArea(int[] height) {
         int maxArea = 0;
-        int aPointer = 0;
-        int bPointer = height.length-1;
-        
-        while(aPointer < bPointer){
-         
-            if(height[aPointer] < height[bPointer]){
-                maxArea = Math.max(maxArea, height[aPointer]*(bPointer-aPointer));
-                aPointer++;
+        int aPointer = 0;    //pointer from left end.
+        int bPointer = height.length-1;  //pointer from right end.
+        //both the pointers are made to move inwards, with maxArea being updated(if required) at each point.        
+        while(aPointer < bPointer){    //until both the pointers meet.
+            if(height[aPointer] < height[bPointer]){ //the smaller height and the area created by it is common to both the heights. hence, the smaller height is considered.    
+                maxArea = Math.max(maxArea, height[aPointer]*(bPointer-aPointer)); //(bPointer-aPointer) is the breadth.
+                aPointer++;  //moving inwards.
             }
-            
             else
             {
                 maxArea = Math.max(maxArea, height[bPointer]*(bPointer-aPointer));
-                bPointer--;
+                bPointer--;  //moving inwards.
             }
-  
         }
         return maxArea;
     }
