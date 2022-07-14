6400862b06777b3e7c9ac4a018ad233f9cb7d0e8
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-07-15 04:17:37 +0530

    Time: 774 ms (26.52%), Space: 352.3 MB (11.79%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.java
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -2,17 +2,17 @@ class Solution {
     public boolean isPalindrome(String s) {
         String fixedString = "";
         
-        for(char ch : s.toCharArray()){
-            if(Character.isLetter(ch) || Character.isDigit(ch))
+        for(char ch : s.toCharArray()){      //string to charArray.
+            if(Character.isLetter(ch) || Character.isDigit(ch))  //the first part checks if its a letter while the second part checks if its a number.(alphanumeric)
                 fixedString = fixedString + ch;
         }
         
-        fixedString = fixedString.toLowerCase();
+        fixedString = fixedString.toLowerCase();            //for ignoring case. can be converted to upper case as well.
         
-        int aPointer = 0;
-        int bPointer = fixedString.length()-1;
+        int aPointer = 0;      //left pointer.
+        int bPointer = fixedString.length()-1;   //right pointer.
         
-        while(aPointer <= bPointer){
+        while(aPointer < bPointer){   //un
             if(fixedString.charAt(aPointer) != fixedString.charAt(bPointer))
                 return false;
             
