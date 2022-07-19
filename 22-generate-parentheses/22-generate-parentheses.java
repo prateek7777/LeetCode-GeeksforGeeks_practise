eb35b23df884e45d0c831ffd65cbd21333cacb6d
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-07-20 00:54:34 +0530

    Time: 3 ms (45.01%), Space: 43.2 MB (75.08%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -9,16 +9,17 @@ class Solution {
     
     public void createParenthesis(int openPCount, int closePCount, int n, String expression, List<String> answer){
         
-        if(openPCount == closePCount && closePCount == n){
+        if(openPCount == closePCount && closePCount == n){          //openPCount=2=closePCount, but n = 3. Thus, closePCount == n.   //base case.
             answer.add(expression);
             return;
         }
         
-        if(openPCount < n)
+        if(openPCount < n)         //obviously.
             createParenthesis(openPCount+1, closePCount, n, expression+"(", answer);
         
-        if(closePCount < openPCount)
+        if(closePCount < openPCount)   //we don't need to validate the string expression. we add the ")" only if there is a corresponding "(".
              createParenthesis(openPCount, closePCount+1, n, expression+")", answer);
+        //the ")" itself is the backtrack.
         
         return;
    
