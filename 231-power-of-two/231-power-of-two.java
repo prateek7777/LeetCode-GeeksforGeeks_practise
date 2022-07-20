58ca49615c18ec887012a9309eaad13ce31a4156
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-07-21 00:50:05 +0530

    Time: 2 ms (68.06%), Space: 41 MB (69.74%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -3,12 +3,12 @@ class Solution {
         if(n <= 0)
             return false;
         
-        if(n == 1)
+        if(n == 1)         //base case and also any number to the power 0 is 1. so 1 applies for all numbers.
             return true;
         
-        if((n % 2 == 0) && (isPowerOfTwo(n/2) == true))
+        if((n % 2 == 0) && (isPowerOfTwo(n/2) == true))        //n%2 will keep on decrementing the number and the function call will keep checking and returning true.
             return true;
         else
-            return false;
+            return false;   //if any one of the condition of line 9 fails, we return false.
     }
 }
