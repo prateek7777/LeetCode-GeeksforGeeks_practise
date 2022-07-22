4a96876f4fa696b808e2b6a0e5d19617f3c43708
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-07-23 04:55:48 +0530

    Time: 1 ms (100.00%), Space: 39.3 MB (97.19%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        
+        if(n <= 0)
+            return false;
+        
+        if(n == 1)          //base case and also any number to the power 0 is 1. so, 1 applies for all numbers.
+            return true;
+        
+        if(( n % 4 == 0 ) && ( isPowerOfFour(n/4) == true )) //if n is divisible by 4and only true is returned in all the subsequent recursive fuction calls. n keeps on getting decremented in each function call.
+            return true;
+           else
+            return false;
+    
+    }
+}
