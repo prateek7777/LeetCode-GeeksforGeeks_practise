e062abebf16622a5ebf3bd8f2404fe8c0485d51b
Prateek J Purohit <63818757+prateek7777@users.noreply.github.com>
2022-07-26 01:53:52 +0530

    Time: 51 ms (33.17%), Space: 54.2 MB (27.76%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -6,18 +6,16 @@ class Solution {
         for(int i=0; i<s.length(); i++)
         {
             char c = s.charAt(i);
-            memo.put(c,memo.getOrDefault(c,0)+1);
-        }
-        
+            memo.put(c,memo.getOrDefault(c,0)+1);        //assign the count(int) of each character to its key(char).
+        }                                                //getOrDefault is used to avoid the runtime error when the HashMap returns null(it returns 0 instead).
         
         for(int i=0; i<s.length(); i++){
-            char c = s.charAt(i);
+            char c = s.charAt(i);             
             
-            if(memo.get(c) == 1)
+            if(memo.get(c) == 1)                //as per the question.
                 return i;
         }
-        
-        return -1;
+        return -1;                                //as per the question.
         
     }
 }
